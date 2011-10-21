ee3b276be5f4e9e18a10875f7752a72666d10337
Andrew Kurochka <aki.viper@gmail.com>
2011-10-21 13:18:04 +0300

    Added predefined configuraiton

--- OutputColorer/Controls/OutputColorerOptionsPage.cs
+++ OutputColorer/Controls/OutputColorerOptionsPage.cs
@@ -7,6 +7,8 @@ using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
 
 namespace Talk2Bits.OutputColorer.Controls
 {
@@ -20,21 +22,11 @@ namespace Talk2Bits.OutputColorer.Controls
             _colorerOptions = new OutputColorerOptions();
             _colorerOptions.Location = new Point(0, 0);
             _colorerOptions.OptionsPage = this;
+
+            _colorerOptions.BuildOuptutSetting = GetBuildOutputDefaultConfiguration();
+            _colorerOptions.DebugOuptutSetting = GetDebugOutputDefaultConfiguration();
         }
 
-        [Browsable(false)]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        protected override IWin32Window Window
-        {
-            get { return _colorerOptions; }
-        }
-
-        protected override void OnApply(PageApplyEventArgs e)
-        {
-            _colorerOptions.Apply(e.ApplyBehavior);
-            base.OnApply(e);
-        }
-        
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         [TypeConverter(typeof(ColorerFormatSettingsCollectionConverter))]
         public Collection<ColorerFormatSetting> BuildOutputSettings
@@ -49,6 +41,160 @@ namespace Talk2Bits.OutputColorer.Controls
         {
             get { return new Collection<ColorerFormatSetting>(_colorerOptions.DebugOuptutSetting.ToList()); }
             set { _colorerOptions.DebugOuptutSetting = value; }
+        }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        protected override IWin32Window Window
+        {
+            get { return _colorerOptions; }
+        }
+
+        protected override void OnApply(PageApplyEventArgs e)
+        {
+            _colorerOptions.Apply(e.ApplyBehavior);
+            base.OnApply(e);
+        }
+
+        public override void ResetSettings()
+        {
+            BuildOutputSettings = GetBuildOutputDefaultConfiguration();
+            DebugOutputSettings = GetDebugOutputDefaultConfiguration();
+        }
+
+        private static Collection<ColorerFormatSetting> GetBuildOutputDefaultConfiguration()
+        {
+            var buildOutputSettings = new Collection<ColorerFormatSetting>();
+
+            buildOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^={10}\sBuild:.*, 0 failed.*$",
+                        ForeColor = Color.HotPink,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.BuildSucceded",
+                        IsBold = true
+                    }
+                );
+
+            buildOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^.+: warning \w*:.+$",
+                        ForeColor = Color.Olive,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.BuildWarning",
+                        IsBold = false
+                    }
+                );
+
+            buildOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^.+: error \w+:.+$",
+                        ForeColor = Color.Red,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.BuildError",
+                        IsBold = true
+                    }
+                );
+            
+            buildOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^={10}\sBuild:.*, [^0]\d? failed.*$",
+                        ForeColor = Color.Red,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.BuildFailed",
+                        IsBold = true
+                    }
+                );
+
+            buildOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^------ Build started:",
+                        ForeColor = Color.Green,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.BuildStarted",
+                        IsBold = true
+                    }
+                );
+
+            return buildOutputSettings;
+        }
+
+        private static Collection<ColorerFormatSetting> GetDebugOutputDefaultConfiguration()
+        {
+            var debugOutputSettings = new Collection<ColorerFormatSetting>();
+
+            debugOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^\'.+\'\s+.+: (?:Loaded|Cannot find or open the PDB file).*$",
+                        ForeColor = Color.Silver,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.DebugLoadPDB",
+                        IsBold = false
+                    }
+                );
+
+            debugOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^The (?:thread|program) .+ has exited with code .+$",
+                        ForeColor = Color.Silver,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.DebugThread",
+                        IsBold = false
+                    }
+                );
+
+            debugOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^.+: error \w+:.+$",
+                        ForeColor = Color.Red,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.BuildError",
+                        IsBold = true
+                    }
+                );
+
+            debugOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^\s+at",
+                        ForeColor = Color.Red,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.DebugException",
+                        IsBold = true
+                    }
+                );
+
+            debugOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^A first chance exception of type",
+                        ForeColor = Color.Red,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.DebugException2",
+                        IsBold = true
+                    }
+                );
+
+            debugOutputSettings.Add(
+                new ColorerFormatSetting
+                    {
+                        Regex = @"^\s*--- End of inner exception stack trace ---",
+                        ForeColor = Color.Red,
+                        BackColor = Color.White,
+                        ClassificationType = "OutputColorer.DebugException3",
+                        IsBold = true
+                    }
+                );
+
+            return debugOutputSettings;
         }
     }
 }
